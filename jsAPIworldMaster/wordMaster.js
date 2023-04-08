36cb111cc58b2da13b392d4579e2e3a3134974af
Michal <sasxcxc@icloud.com>
2023-04-08 17:54:50 +0200

    learning of js

--- jsAPIworldMaster/wordMaster.js
+++ jsAPIworldMaster/wordMaster.js
@@ -1,38 +1,45 @@
+counter = 0
+const ignoreOtherSymbols = document.querySelector(".word")
 function isLetter(letter) {
+    counter++
     return /^[a-zA-Z]$/.test(letter)
   }
-document.querySelector("input").addEventListener("keydown", function (event) {
+  ignoreOtherSymbols.addEventListener("keydown", function (event) {
   if (!isLetter(event.key)) {
     event.preventDefault()
   }
 })
-counter = 0
 const container = document.getElementsByClassName("firstWord")[0]
-container.onkeyup = function(e) {
-    let target = e.srcElement || e.target;
+container.onkeyup = function(event) {
+    let target = event.srcElement || event.target;
     let myLength = target.value.length;
-    if (target.nextElementSibling && myLength === 1) {
+    if (myLength === 1) {
         let next = target
         while (next = next.nextElementSibling) {
-            if (next.tagName.toLowerCase() === "input") {
-                next.focus()
-                counter++
-                break;
+            if (isLetter(event.key)) {
+                if (next.tagName.toLowerCase() === "input") {
+                    if (isLetter(event.key)) {
+                        next.focus()
+                    }
+                    break
+                }
             }
         }
     }
-    else if (target.previousElementSibling && myLength === 0) {
+    else if (myLength === 0) {
         let previous = target
         while (previous = previous.previousElementSibling) {
             if (previous.tagName.toLowerCase() === "input") {
-                previous.focus()
-                counter--
-                break;
+                if (isLetter(event.key)) {
+                    previous.focus()
+                    counter--
+                } else if (event.key === 'Backspace') {
+                    previous.value = ''
+                    previous.focus()
+                    counter--
+                }
             }
-        }
-    }
-    else if (!target.previousElementSibling && myLength === 1) {
-        e.preventDefault()
+     }
     }
     console.log(counter)
 }
