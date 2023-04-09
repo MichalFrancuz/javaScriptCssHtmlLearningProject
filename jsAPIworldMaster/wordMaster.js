75ced69f2cbce8acba9ec21fa6e266022b839737
Michal <sasxcxc@icloud.com>
2023-04-09 23:08:26 +0200

    Learning of javascript

--- jsAPIworldMaster/wordMaster.js
+++ jsAPIworldMaster/wordMaster.js
@@ -1,7 +1,8 @@
-counter = 0
 const ignoreOtherSymbols = document.querySelector(".word")
+let counter = 1
+let word = ''
+let container = document.getElementsByClassName("firstWord")[0]
 function isLetter(letter) {
-    counter++
     return /^[a-zA-Z]$/.test(letter)
   }
   ignoreOtherSymbols.addEventListener("keydown", function (event) {
@@ -9,37 +10,43 @@ function isLetter(letter) {
     event.preventDefault()
   }
 })
-const container = document.getElementsByClassName("firstWord")[0]
-container.onkeyup = function(event) {
+container.onkeydown = function(event) {
     let target = event.srcElement || event.target;
     let myLength = target.value.length;
+    if (isLetter(event.key) && counter === 5) {
+            target.value = target.value.slice(0, -1)
+            word += target.value
+    }
+    if (isLetter(event.key)) {
+        word += target.value
+    }
+    if (event.key === 'Backspace') {
+        word += target.value.slice(0, 1)
+        target.value = ''
+    }
     if (myLength === 1) {
-        let next = target
-        while (next = next.nextElementSibling) {
-            if (isLetter(event.key)) {
-                if (next.tagName.toLowerCase() === "input") {
-                    if (isLetter(event.key)) {
-                        next.focus()
-                    }
-                    break
+        while (target = target.nextElementSibling) {
+            if (target.tagName.toLowerCase() === "input") {
+                if (isLetter(event.key)) {
+                    target.focus()
+                    counter++
                 }
+                break
             }
         }
     }
-    else if (myLength === 0) {
-        let previous = target
-        while (previous = previous.previousElementSibling) {
-            if (previous.tagName.toLowerCase() === "input") {
-                if (isLetter(event.key)) {
-                    previous.focus()
+    else {
+        while (target = target.previousElementSibling) {
+            if (target.tagName.toLowerCase() === "input") {
+                if (event.key === 'Backspace') {
+                    target.focus()
                     counter--
-                } else if (event.key === 'Backspace') {
-                    previous.value = ''
-                    previous.focus()
-                    counter--
+                    target.value = ''
                 }
+                break
             }
      }
     }
     console.log(counter)
+    console.log(word)
 }
