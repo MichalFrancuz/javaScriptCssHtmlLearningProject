7415171f20ef0735761646e2ce3f3dab91bb6c7a
Michal <sasxcxc@icloud.com>
2023-04-13 19:59:00 +0200

    learning of js

--- jsAPIworldMaster/wordMaster.js
+++ jsAPIworldMaster/wordMaster.js
@@ -1,6 +1,8 @@
 const ignoreOtherSymbols = document.querySelector(".word")
 let counter = 1
 let word = ''
+let newWord = ''
+let inputss = 'a'
 let container = document.getElementsByClassName("firstWord")[0]
 function isLetter(letter) {
     return /^[a-zA-Z]$/.test(letter)
@@ -10,17 +12,17 @@ function isLetter(letter) {
     event.preventDefault()
   }
 })
-container.onkeyup = function(event) {
+container.onkeydown = function(event) {
     let target = event.srcElement || event.target;
     let myLength = target.value.length;
     if (isLetter(event.key) && counter === 5) {
             target.value = target.value.slice(0, -1)
     }
-    if (isLetter(event.key) && counter < 5) {
+    if (isLetter(event.key)) {
         word += target.value
     }
     if (event.key === 'Backspace') {
-        word -= target.value.slice(0, word.length - 1)
+        word -= target.value.slice(0, -1)
         target.value = ''
     }
     if (myLength === 1) {
@@ -49,3 +51,17 @@ container.onkeyup = function(event) {
     console.log(counter)
     console.log(word)
 }
+function saveInputs() {
+    newWord = ''
+    for (let i = 0; i < 5; i++) {
+      newWord += container[i].value
+    }
+    return newWord.trim()
+}
+document.addEventListener("keydown", function(event) {
+    if (event.key === 'Enter') {
+        inputss = saveInputs()
+      console.log("Enter was pressed!");
+    }
+  });
+  console.log(inputss)
