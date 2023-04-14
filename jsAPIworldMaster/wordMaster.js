35fbef65053beb09aba167f82ce457dea1d4050c
Michal <sasxcxc@icloud.com>
2023-04-14 05:36:39 +0200

    I was ended handling correctly key 'Enter' and 'Backspace'

--- jsAPIworldMaster/wordMaster.js
+++ jsAPIworldMaster/wordMaster.js
@@ -1,9 +1,10 @@
 const ignoreOtherSymbols = document.querySelector(".word")
 let counter = 1
 let word = ''
-let newWord = ''
+let fifthValue = ''
 let inputss = 'a'
 let container = document.getElementsByClassName("firstWord")[0]
+let container2 = document.getElementsByClassName("lastFirstWord")[0]
 function isLetter(letter) {
     return /^[a-zA-Z]$/.test(letter)
   }
@@ -16,14 +17,21 @@ container.onkeydown = function(event) {
     let target = event.srcElement || event.target;
     let myLength = target.value.length;
     if (isLetter(event.key) && counter === 5) {
-            target.value = target.value.slice(0, -1)
+        target.value = target.value.slice(0, -1)
     }
     if (isLetter(event.key)) {
         word += target.value
     }
+    if (event.key === 'Enter') {
+        fifthValue = target.value
+        var wordArray = word.split('')
+        wordArray[4] = fifthValue
+        var newWord = wordArray.join('')
+        console.log(newWord)
+        }
     if (event.key === 'Backspace') {
-        word -= target.value.slice(0, -1)
         target.value = ''
+        word = word.slice(0, -1)
     }
     if (myLength === 1) {
         while (target = target.nextElementSibling) {
@@ -51,17 +59,3 @@ container.onkeydown = function(event) {
     console.log(counter)
     console.log(word)
 }
-function saveInputs() {
-    newWord = ''
-    for (let i = 0; i < 5; i++) {
-      newWord += container[i].value
-    }
-    return newWord.trim()
-}
-document.addEventListener("keydown", function(event) {
-    if (event.key === 'Enter') {
-        inputss = saveInputs()
-      console.log("Enter was pressed!");
-    }
-  });
-  console.log(inputss)
