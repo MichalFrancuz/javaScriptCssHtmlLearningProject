5b771258bf84c0af5fb68dc5ed11fa97533ab245
Michal <sasxcxc@icloud.com>
2023-04-17 22:58:42 +0200

    learning of javascript

--- jsAPIworldMaster/wordMaster.js
+++ jsAPIworldMaster/wordMaster.js
@@ -3,8 +3,10 @@ let counter = 1
 let word = ''
 let fifthValue = ''
 let inputss = 'a'
-let container = document.getElementsByClassName("firstWord")[0]
-let container2 = document.getElementsByClassName("firstWord")[0]
+let row = 0
+let container = document.getElementsByClassName("word")[row]
+let container2 = document.getElementsByClassName("word")[row]
+const inputs = document.getElementsByClassName('input')
 function isLetter(letter) {
     return /^[a-zA-Z]$/.test(letter)
   }
@@ -13,15 +15,49 @@ function isLetter(letter) {
     event.preventDefault()
   }
 })
-container2.onkeyup = function(event) {
+container.onkeyup = function(event) {
     let target = event.srcElement || event.target
     let myLength = target.value.length
-    if (isLetter(event.key)) {
+    if (isLetter(event.key) && counter < 5) {
         word += target.value
     }
-    // if (isLetter(event.key) && counter === 5) {
-    //     target.value = target.value.slice(0, -1)
-    // }
+    if (event.key === 'Enter' && counter == 5) {
+        fifthValue = target.value
+        const wordArray = word.split('')
+        wordArray[4] = fifthValue
+        let newWord = wordArray.join('')
+        console.log(row)
+        console.log(newWord)
+        if (document.getElementsByClassName("word")[row]) {
+            counter = 1
+            word = ''
+            fifthValue = ''
+            row++
+            container = document.getElementsByClassName('word')[row]
+            container2 = document.getElementsByClassName('word')[row]
+            if (container) {
+                inputs[0].focus() //here is problem with index - this index don't exist 
+            }
+        }
+        const apiUrl = 'https://words.dev-apis.com/word-of-the-day?puzzle=1337'
+        fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+        console.log(data);
+        if (data.word === newWord) {
+            console.log('You win')
+        } 
+        else {
+            console.log('You lose')
+        }
+        })
+        .catch(error => {
+            console.error(error)
+        })
+        }
+    if (event.key === 'Backspace') {
+        target.value = ''
+    }
     if (myLength === 1) {
         while (target = target.nextElementSibling) {
             if (target.tagName.toLowerCase() === "input") {
@@ -33,76 +69,26 @@ container2.onkeyup = function(event) {
             }
         }
     }
-    else {
+    console.log(counter)
+    console.log(word)
+}
+container2.onkeydown = function(event) {
+    let target = event.srcElement || event.target
+    let myLength = target.value.length
+    if (isLetter(event.key) && counter === 5) {
+        target.value = target.value.slice(0, -1)
+    }
+    if (myLength === 0) {
         while (target = target.previousElementSibling) {
             if (target.tagName.toLowerCase() === "input") {
                 if (event.key === 'Backspace') {
                     target.focus()
                     counter--
                     target.value = ''
+                    word = word.slice(0, -1)
                 }
                 break
             }
      }
     }
-}
-container.onkeydown = function(event) {
-    let target = event.srcElement || event.target;
-    // let myLength = target.value.length;
-    if (isLetter(event.key) && counter === 5) {
-        target.value = target.value.slice(0, -1)
-    }
-    // if (isLetter(event.key)) {
-    //     word += target.value
-    // }
-    if (event.key === 'Enter' && counter == 5) {
-        fifthValue = target.value
-        var wordArray = word.split('')
-        wordArray[4] = fifthValue
-        let newWord = wordArray.join('')
-        const apiUrl = 'https://words.dev-apis.com/word-of-the-day?puzzle=1337';
-        fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-        console.log(data);
-        if (data.word === newWord) {
-            console.log('You win')
-        } else {
-            console.log('You lose')
-        }
-        })
-        .catch(error => {
-            console.error(error);
-        });
-        console.log(newWord)
-        }
-    if (event.key === 'Backspace') {
-        target.value = ''
-        word = word.slice(0, -1)
-    }
-    // if (myLength === 1) {
-    //     while (target = target.nextElementSibling) {
-    //         if (target.tagName.toLowerCase() === "input") {
-    //             if (isLetter(event.key)) {
-    //                 target.focus()
-    //                 counter++
-    //             }
-    //             break
-    //         }
-    //     }
-    // }
-    // else {
-    //     while (target = target.previousElementSibling) {
-    //         if (target.tagName.toLowerCase() === "input") {
-    //             if (event.key === 'Backspace') {
-    //                 target.focus()
-    //                 counter--
-    //                 target.value = ''
-    //             }
-    //             break
-    //         }
-    //  }
-    // }
-    console.log(counter)
-    console.log(word)
 }
